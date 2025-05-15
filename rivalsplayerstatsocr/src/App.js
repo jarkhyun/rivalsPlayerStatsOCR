8516756a02e935f3643a8eda8437ee7fbc67f14a
Ryan Ku <rjhk@uw.edu>
2025-05-15 01:46:07 -0700

    implemented ctr instead of for loop that was causing recursion issues

--- rivalsplayerstatsocr/src/App.js
+++ rivalsplayerstatsocr/src/App.js
@@ -81,7 +81,10 @@ function App() {
   const searchNames = () => {
     const arr = [];
     const arr2 =[];
-    const arr3 = []
+    const arr3 = [];
+    let ctr = 0;
+    setPlayers([]);
+    setHeroes([]);
     for (let i = 0; i < 6; i++) {
       setIsLoading(true);
       arr[i] = document.getElementById(`player${i + 1}`).value;
@@ -98,45 +101,50 @@ function App() {
         .then(data => {
           // handle success
           arr2.push(data);
-
           let fst = {play_time: -Infinity}, sec = {play_time: -Infinity}, thd = {play_time: -Infinity};
-          for (let i = 0; i < arr2.length; i++) {
-            arr2[i].heroes_ranked.forEach(x => {
-            // If current element is greater than fst
-            if (x.play_time > fst.play_time) {
-              thd = sec;
-              sec = fst;
-              fst = x;
-            }
-            // If x is between fst and sec
-            else if (x.play_time > sec.play_time && x.play_time !== fst.play_time) {
-              thd = sec;
-              sec = x;
-            }
-            // If x is between sec and thd
-            else if (x.play_time > thd.play_time && x.play_time !== sec && x.play_time !== fst.play_time) {
-                thd = x;
-            }
-          })
-          let res = [];
-          if (fst.play_time !== -Infinity) res.push(fst);
-          if (sec.play_time !== -Infinity) res.push(sec);
-          if (thd.play_time !== -Infinity) res.push(thd); 
-          arr3.push(res);
-        }
-        setPlayers(arr2);
-        setHeroes(arr3);
+          
+          arr2[ctr].heroes_ranked.forEach(x => {
+          // If current element is greater than fst
+          if (x.play_time > fst.play_time) {
+            thd = sec;
+            sec = fst;
+            fst = x;
+          }
+          // If x is between fst and sec
+          else if (x.play_time > sec.play_time && x.play_time !== fst.play_time) {
+            thd = sec;
+            sec = x;
+          }
+          // If x is between sec and thd
+          else if (x.play_time > thd.play_time && x.play_time !== sec && x.play_time !== fst.play_time) {
+              thd = x;
+          }
         })
+        let res = [];
+        if (fst.play_time !== -Infinity) res.push(fst);
+        if (sec.play_time !== -Infinity) res.push(sec);
+        if (thd.play_time !== -Infinity) res.push(thd); 
+        arr3.push(res);
+        ctr++;
+        }
+        
+        // console.log(players)
+        // console.log(heroes)
+        )
         .catch(function (error) {
           // handle error
           console.log(error);
         })
         .finally(function () {
           // always executed
-          setIsLoading(false);
+          setIsLoading(false); 
         });
       }
     };
+    setPlayers(arr2);
+    setHeroes(arr3);
+    console.log(players)
+    console.log(heroes);
   }
 
   return (
@@ -164,49 +172,65 @@ function App() {
           {isLoading !== true && (
             <>
               {players[0] && 
-              <div id="player">
-                {players[0] && players[0].name}
-                {players[0].overall_stats.ranked.total_matches && <div>Overall Ranked Winrate: {(players[0].overall_stats.ranked.total_wins / players[0].overall_stats.ranked.total_matches * 100).toFixed(2) }%</div>}
-                <div>Top Heroes: </div>
-                {heroes[0][0] && <div>1. {heroes[0][0].hero_name} : {heroes[0][0].wins} wins of {heroes[0][0].matches} matches. {(heroes[0][0].wins / heroes[0][0].matches * 100).toFixed(2) }%</div>}
-                {heroes[0][1] && <div>2. {heroes[0][1].hero_name} : {heroes[0][1].wins} wins of {heroes[0][1].matches} matches. {(heroes[0][1].wins / heroes[0][1].matches * 100).toFixed(2) }%</div>}
-                {heroes[0][2] && <div>3. {heroes[0][2].hero_name} : {heroes[0][2].wins} wins of {heroes[0][2].matches} matches. {(heroes[0][2].wins / heroes[0][2].matches * 100).toFixed(2) }%</div>} 
-                
-                {/* {players[1] && players[1].name}
-                {players[1].overall_stats.ranked.total_matches && <div>Overall Ranked Winrate: {(players[1].overall_stats.ranked.total_wins / players[1].overall_stats.ranked.total_matches * 100).toFixed(2) }%</div>}
-                <div>Top Heroes: </div>
-                {heroes[1][0] && <div>1. {heroes[1][0].hero_name} : {heroes[1][0].wins} wins of {heroes[1][0].matches} matches. {(heroes[1][0].wins / heroes[1][0].matches * 100).toFixed(2) }%</div>}
-                {heroes[1][1] && <div>2. {heroes[1][1].hero_name} : {heroes[1][1].wins} wins of {heroes[1][1].matches} matches. {(heroes[1][1].wins / heroes[1][1].matches * 100).toFixed(2) }%</div>}
-                {heroes[1][2] && <div>3. {heroes[1][2].hero_name} : {heroes[1][2].wins} wins of {heroes[1][2].matches} matches. {(heroes[1][2].wins / heroes[1][2].matches * 100).toFixed(2) }%</div>} 
-
-                {players[2] && players[2].name}
-                {players[2].overall_stats.ranked.total_matches && <div>Overall Ranked Winrate: {(players[2].overall_stats.ranked.total_wins / players[2].overall_stats.ranked.total_matches * 100).toFixed(2) }%</div>}
-                <div>Top Heroes: </div>
-                {heroes[2][0] && <div>1. {heroes[2][0].hero_name} : {heroes[2][0].wins} wins of {heroes[2][0].matches} matches. {(heroes[2][0].wins / heroes[2][0].matches * 100).toFixed(2) }%</div>}
-                {heroes[2][1] && <div>2. {heroes[2][1].hero_name} : {heroes[2][1].wins} wins of {heroes[2][1].matches} matches. {(heroes[2][1].wins / heroes[2][1].matches * 100).toFixed(2) }%</div>}
-                {heroes[2][2] && <div>3. {heroes[2][2].hero_name} : {heroes[2][2].wins} wins of {heroes[2][2].matches} matches. {(heroes[2][2].wins / heroes[2][2].matches * 100).toFixed(2) }%</div>} 
-
-                {players[3] && players[3].name}
-                {players[3].overall_stats.ranked.total_matches && <div>Overall Ranked Winrate: {(players[3].overall_stats.ranked.total_wins / players[3].overall_stats.ranked.total_matches * 100).toFixed(2) }%</div>}
-                <div>Top Heroes: </div>
-                {heroes[3][0] && <div>1. {heroes[3][0].hero_name} : {heroes[3][0].wins} wins of {heroes[3][0].matches} matches. {(heroes[3][0].wins / heroes[3][0].matches * 100).toFixed(2) }%</div>}
-                {heroes[3][1] && <div>2. {heroes[3][1].hero_name} : {heroes[3][1].wins} wins of {heroes[3][1].matches} matches. {(heroes[3][1].wins / heroes[3][1].matches * 100).toFixed(2) }%</div>}
-                {heroes[3][2] && <div>3. {heroes[3][2].hero_name} : {heroes[3][2].wins} wins of {heroes[3][2].matches} matches. {(heroes[3][2].wins / heroes[3][2].matches * 100).toFixed(2) }%</div>} 
-
-                {players[4] && players[4].name}
-                {players[4].overall_stats.ranked.total_matches && <div>Overall Ranked Winrate: {(players[4].overall_stats.ranked.total_wins / players[4].overall_stats.ranked.total_matches * 100).toFixed(2) }%</div>}
-                <div>Top Heroes: </div>
-                {heroes[4][0] && <div>1. {heroes[4][0].hero_name} : {heroes[4][0].wins} wins of {heroes[4][0].matches} matches. {(heroes[4][0].wins / heroes[4][0].matches * 100).toFixed(2) }%</div>}
-                {heroes[4][1] && <div>2. {heroes[4][1].hero_name} : {heroes[4][1].wins} wins of {heroes[4][1].matches} matches. {(heroes[4][1].wins / heroes[4][1].matches * 100).toFixed(2) }%</div>}
-                {heroes[4][2] && <div>3. {heroes[4][2].hero_name} : {heroes[4][2].wins} wins of {heroes[4][2].matches} matches. {(heroes[4][2].wins / heroes[4][2].matches * 100).toFixed(2) }%</div>} 
-                
-                {players[5] && players[5].name}
-                {players[5].overall_stats.ranked.total_matches && <div>Overall Ranked Winrate: {(players[5].overall_stats.ranked.total_wins / players[5].overall_stats.ranked.total_matches * 100).toFixed(2) }%</div>}
-                <div>Top Heroes: </div>
-                {heroes[5][0] && <div>1. {heroes[5][0].hero_name} : {heroes[5][0].wins} wins of {heroes[5][0].matches} matches. {(heroes[5][0].wins / heroes[5][0].matches * 100).toFixed(2) }%</div>}
-                {heroes[5][1] && <div>2. {heroes[5][1].hero_name} : {heroes[5][1].wins} wins of {heroes[5][1].matches} matches. {(heroes[5][1].wins / heroes[5][1].matches * 100).toFixed(2) }%</div>}
-                {heroes[5][2] && <div>3. {heroes[5][2].hero_name} : {heroes[5][2].wins} wins of {heroes[5][2].matches} matches. {(heroes[5][2].wins / heroes[5][2].matches * 100).toFixed(2) }%</div>}  */}
-              </div>}
+                <div>
+                  {players[0].name}
+                  {players[0].overall_stats.ranked.total_matches && <div>Overall Ranked Winrate: {(players[0].overall_stats.ranked.total_wins / players[0].overall_stats.ranked.total_matches * 100).toFixed(2) }%</div>}
+                  <div>Top Heroes: </div>
+                  {heroes[0][0] && <div>1. {heroes[0][0].hero_name} : {heroes[0][0].wins} wins of {heroes[0][0].matches} matches. {(heroes[0][0].wins / heroes[0][0].matches * 100).toFixed(2) }%</div>}
+                  {heroes[0][1] && <div>2. {heroes[0][1].hero_name} : {heroes[0][1].wins} wins of {heroes[0][1].matches} matches. {(heroes[0][1].wins / heroes[0][1].matches * 100).toFixed(2) }%</div>}
+                  {heroes[0][2] && <div>3. {heroes[0][2].hero_name} : {heroes[0][2].wins} wins of {heroes[0][2].matches} matches. {(heroes[0][2].wins / heroes[0][2].matches * 100).toFixed(2) }%</div>} 
+                </div>
+              }
+              {players[1] && 
+                <div>
+                  {players[1].name}
+                  {players[1].overall_stats.ranked.total_matches && <div>Overall Ranked Winrate: {(players[1].overall_stats.ranked.total_wins / players[1].overall_stats.ranked.total_matches * 100).toFixed(2) }%</div>}
+                  <div>Top Heroes: </div>
+                  {heroes[1][0] && <div>1. {heroes[1][0].hero_name} : {heroes[1][0].wins} wins of {heroes[1][0].matches} matches. {(heroes[1][0].wins / heroes[1][0].matches * 100).toFixed(2) }%</div>}
+                  {heroes[1][1] && <div>2. {heroes[1][1].hero_name} : {heroes[1][1].wins} wins of {heroes[1][1].matches} matches. {(heroes[1][1].wins / heroes[1][1].matches * 100).toFixed(2) }%</div>}
+                  {heroes[1][2] && <div>3. {heroes[1][2].hero_name} : {heroes[1][2].wins} wins of {heroes[1][2].matches} matches. {(heroes[1][2].wins / heroes[1][2].matches * 100).toFixed(2) }%</div>} 
+                </div>
+              }
+              {players[2] && 
+                <div>
+                  {players[2].name}
+                  {players[2].overall_stats.ranked.total_matches && <div>Overall Ranked Winrate: {(players[2].overall_stats.ranked.total_wins / players[2].overall_stats.ranked.total_matches * 100).toFixed(2) }%</div>}
+                  <div>Top Heroes: </div>
+                  {heroes[2][0] && <div>1. {heroes[2][0].hero_name} : {heroes[2][0].wins} wins of {heroes[2][0].matches} matches. {(heroes[2][0].wins / heroes[2][0].matches * 100).toFixed(2) }%</div>}
+                  {heroes[2][1] && <div>2. {heroes[2][1].hero_name} : {heroes[2][1].wins} wins of {heroes[2][1].matches} matches. {(heroes[2][1].wins / heroes[2][1].matches * 100).toFixed(2) }%</div>}
+                  {heroes[2][2] && <div>3. {heroes[2][2].hero_name} : {heroes[2][2].wins} wins of {heroes[2][2].matches} matches. {(heroes[2][2].wins / heroes[2][2].matches * 100).toFixed(2) }%</div>} 
+                </div>
+              } 
+              {players[3] && 
+                <div>
+                  {players[3].name}
+                  {players[3].overall_stats.ranked.total_matches && <div>Overall Ranked Winrate: {(players[3].overall_stats.ranked.total_wins / players[3].overall_stats.ranked.total_matches * 100).toFixed(2) }%</div>}
+                  <div>Top Heroes: </div>
+                  {heroes[3][0] && <div>1. {heroes[3][0].hero_name} : {heroes[3][0].wins} wins of {heroes[3][0].matches} matches. {(heroes[3][0].wins / heroes[3][0].matches * 100).toFixed(2) }%</div>}
+                  {heroes[3][1] && <div>2. {heroes[3][1].hero_name} : {heroes[3][1].wins} wins of {heroes[3][1].matches} matches. {(heroes[3][1].wins / heroes[3][1].matches * 100).toFixed(2) }%</div>}
+                  {heroes[3][2] && <div>3. {heroes[3][2].hero_name} : {heroes[3][2].wins} wins of {heroes[3][2].matches} matches. {(heroes[3][2].wins / heroes[3][2].matches * 100).toFixed(2) }%</div>} 
+                </div>
+              }
+              {players[4] && 
+                <div>
+                  {players[4].name}
+                  {players[4].overall_stats.ranked.total_matches && <div>Overall Ranked Winrate: {(players[4].overall_stats.ranked.total_wins / players[4].overall_stats.ranked.total_matches * 100).toFixed(2) }%</div>}
+                  <div>Top Heroes: </div>
+                  {heroes[4][0] && <div>1. {heroes[4][0].hero_name} : {heroes[4][0].wins} wins of {heroes[4][0].matches} matches. {(heroes[4][0].wins / heroes[4][0].matches * 100).toFixed(2) }%</div>}
+                  {heroes[4][1] && <div>2. {heroes[4][1].hero_name} : {heroes[4][1].wins} wins of {heroes[4][1].matches} matches. {(heroes[4][1].wins / heroes[4][1].matches * 100).toFixed(2) }%</div>}
+                  {heroes[4][2] && <div>3. {heroes[4][2].hero_name} : {heroes[4][2].wins} wins of {heroes[4][2].matches} matches. {(heroes[4][2].wins / heroes[4][2].matches * 100).toFixed(2) }%</div>} 
+                </div>
+              }
+              {players[5] && 
+                <div>
+                  {players[5].name}
+                  {players[5].overall_stats.ranked.total_matches && <div>Overall Ranked Winrate: {(players[5].overall_stats.ranked.total_wins / players[5].overall_stats.ranked.total_matches * 100).toFixed(2) }%</div>}
+                  <div>Top Heroes: </div>
+                  {heroes[5][0] && <div>1. {heroes[5][0].hero_name} : {heroes[5][0].wins} wins of {heroes[5][0].matches} matches. {(heroes[5][0].wins / heroes[5][0].matches * 100).toFixed(2) }%</div>}
+                  {heroes[5][1] && <div>2. {heroes[5][1].hero_name} : {heroes[5][1].wins} wins of {heroes[5][1].matches} matches. {(heroes[5][1].wins / heroes[5][1].matches * 100).toFixed(2) }%</div>}
+                  {heroes[5][2] && <div>3. {heroes[5][2].hero_name} : {heroes[5][2].wins} wins of {heroes[5][2].matches} matches. {(heroes[5][2].wins / heroes[5][2].matches * 100).toFixed(2) }%</div>} 
+                </div> 
+              }
             </>
           )}
         </div>
